ea16ab32b94806ae22fd04def70fb9d39f820387
victorhqc <victorhqc@gmail.com>
2016-04-11 17:58:07 -0500

    Search kinda working

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,23 +6,113 @@ import React from 'react';
 let itexicoImage = require('../images/itexico.png');
 let reactImage = require('../images/react.png');
 
-class InnerComponent extends React.Component {
+const users =  [
+    {
+        id: 1,
+        name: 'Victor',
+        email: 'vquiroz@itexico.net'
+    },
+    {
+        id: 2,
+        name: 'Antares',
+        email: 'afarias@itexico.net'
+    },
+    {
+        id: 3,
+        name: 'Abraham',
+        email: 'apanduro@itexico.net'
+    },
+    {
+        id: 4,
+        name: 'Juan Perrito',
+        email: 'juan@perrito.com'
+    }
+];
 
-    handleClick() {
-        // Some other logic goes here
-        this.props.clickedShit();
+class ListComponent extends React.Component {
+
+    chosenList(e) {
+        this.props.chosenMiddle(e.target);
     }
 
-    shouldComponentUpdate() {
-        if(this.props.name === nextProps.name) { return false; }
-
-        return true;
+    renderChildren() {
+        return this.props.data.map((element, i) => {
+            return <li
+                id={i}
+                onClick={this.chosenList.bind(this)}
+                key={i}>{element.name}</li>;
+        });
     }
 
     render() {
         return (
-            <div onClick={this.handleClick.bind(this)}>
-                {this.props.name}
+            <ul className="list-component">
+                {this.renderChildren()}
+            </ul>
+        );
+    }
+}
+
+class SearchComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {users: []};
+    }
+
+    searchInUsers(needle) {
+        return new Promise((resolve, reject) => {
+            window.setTimeout(() => {
+                let rx = new RegExp(needle, 'gi');
+
+                let found = users.filter((user) => {
+                    if(user.name.match(rx)) {
+                        return user;
+                    }
+                });
+
+                if(found.length === 0) {
+                    return reject({
+                        error: 'User Not Found'
+                    });
+                }
+
+                return resolve(found);
+            }, 100);
+        });
+    }
+
+    search(e) {
+        let needle = this.refs.search.value;
+
+        this.searchInUsers(needle)
+        .then(users => {
+            this.setState({users: users});
+        })
+        .catch(e => {
+            this.setState({users: []});
+            console.log(e)
+        });
+    }
+
+    render() {
+        return (
+            <div className="search-component">
+                <input
+                    className="form-control"
+                    ref="search"
+                    type="text" />
+                <button
+                    className="btn btn-success"
+                    onClick={this.search.bind(this)}>
+                    Search
+                </button>
+                <br />
+                <ListComponent
+                    data={this.state.users}
+                    chosenMiddle={this.props.chosenMain}
+                    />
             </div>
         );
     }
@@ -38,57 +128,24 @@ class AppComponent extends React.Component {
         };
     }
 
-    detectMouseMove() {
-        let elm = document.querySelector('#main-container');
-        elm.addEventListener('mousemove', target => {
-            this.setState({
-                x: target.layerX,
-                y: target.layerY
-            });
-        }, false);
+    handleSubmit(e) {
+        e.preventDefault();
     }
 
-    componentDidMount() {
-        this.detectMouseMove();
-    }
-
-    click() {
-        console.log('Im clicked');
+    getUser(user) {
+        console.log('user', user);
     }
 
     render() {
-        console.log('yo! Im rendered again!');
-
-        let x = this.state.x,
-            y = this.state.y,
-            style = {
-                position: 'absolute',
-                transform: 'translate3d(' + x + 'px, ' + y + 'px, 0)'
-            };
 
         return (
-            <div className="container" id="main-container" style={{minWdth: 800, minHeight: 800}}>
-                <div className="row">
-                    <div className="col-sm-6">
-                        <image src={itexicoImage} alt="iTexico Logo" height="150"/>
-                    </div>
-                </div>
-                <div>
-                    <InnerComponent
-                        name="demo"
-                        clickedShit={this.click.bind(this)} />
-                </div>
-                <div className="jumbotron" style={style} id="jumbotron">
-                    <div className="row">
-                        <div className="col-sm-10">
-                            <h1>Welcome to React Course</h1>
-                        </div>
-                        <div className="col-sm-2">
-                            <image src={reactImage} alt="React Logo" height="150"/>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div className="container">
+            <form onSubmit={this.handleSubmit.bind(this)}>
+                <h2>Search for Users</h2>
+                <SearchComponent
+                    chosenMain={this.getUser.bind(this)}/>
+            </form>
+        </div>
         );
     }
 }
